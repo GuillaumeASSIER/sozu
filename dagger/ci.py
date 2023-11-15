c4955946d8ca27f035708c376efa12d672156408
Guillaume Assier <18405490+Sykursen@users.noreply.github.com>
2023-11-15 12:02:53 +0000

    Change img

--- dagger/ci.py
+++ dagger/ci.py
@@ -5,12 +5,14 @@ import dagger
 async def test():
     config = dagger.Config(log_output=sys.stdout)
 
+    src = client.host().directory(".")
+
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         # get reference to the local project
         src = client.host().directory(".")
 
         python = (
-            client.container().from_("rust:1.70.0-bookworm")
+            client.container().from_("cimg/rust:1.70")
             # Install protbuf requirement
             .with_exec(["apt-get", "update"])
             .with_exec(["apt-get", "install", "-y", "protobuf-compiler"])
