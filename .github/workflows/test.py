61a2f97903e00634d62b0ed1ffbb223da535dab9
Guillaume Assier <18405490+Sykursen@users.noreply.github.com>
2023-11-15 10:45:39 +0000

    Init dagger

---
+++ .github/workflows/test.py
@@ -0,0 +1,27 @@
+import sys
+import anyio
+import dagger
+
+async def test():
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+        # get reference to the local project
+        src = client.host().directory(".")
+
+        python = (
+            client.container().from_("rust:1.70")
+            # mount cloned repository into image
+            .with_directory("/", src)
+            # set current working directory for next commands
+            .with_workdir("/")
+            # Build sozu
+            .with_exec(["cargo", "build", "--release"])
+            # Run e2e tests
+            .with_exec(["cargo", "test"])
+        )
+
+        # execute
+        await python.sync()
+
+    print("Tests succeeded!")
+
+anyio.run(test)
