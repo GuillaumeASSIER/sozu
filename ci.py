ad55f75d74f51d0e56979eb4c3c3c01e8015779e
GuillaumeASSIER <guillaume.assier@clever-cloud.com>
2023-11-22 14:40:56 +0100

    WIP Latest changes

--- ci.py
+++ ci.py
@@ -4,6 +4,11 @@ from datetime import datetime
 import anyio
 import dagger
 
+SOZU_CONFIG = """
+
+
+"""
+
 
 async def test():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
@@ -36,7 +41,7 @@ async def test():
             # Cache
             .with_mounted_cache("~/.cargo/registry", client.cache_volume("container_receiver_registry"))
             .with_mounted_cache("~/.cargo/git", client.cache_volume("container_receiver_git"))
-            #.with_mounted_cache("/lagging_server/target", client.cache_volume("container_receiver_target"))
+            .with_mounted_cache("/lagging_server/target", client.cache_volume("container_receiver_target"))
             # Build
             .with_workdir("/app")
             .with_exec(["cargo", "build", "--release"])
@@ -91,10 +96,10 @@ async def test():
             # Retrieve executable
             .with_file("/bin/sozu", container_sozu.file("/app/target/release/sozu"))
             # Mount CI
-            .with_directory("/app", await client.host().directory(".", include=["ci/test.toml","**/ci"]))
+            .with_directory("/app", await client.host().directory(".", include=["ci/test.toml"]))
             .with_workdir("/app")
-            #.with_exec(["openssl", "req", "-newkey","rsa:2048","-nodes","-keyout","ci/sozu.io.key","-out","ci/sozu.io.csr","-subj","/CN=sozu.io"])
-            #.with_exec(["openssl", "x509", "-signkey","ci/sozu.io.key","-in","ci/sozu.io.csr","-req","-days","365","-out","ci/sozu.io.pem"])
+            .with_exec(["openssl", "req", "-newkey","rsa:2048","-nodes","-keyout","ci/sozu.io.key","-out","ci/sozu.io.csr","-subj","/CN=sozu.io"])
+            .with_exec(["openssl", "x509", "-signkey","ci/sozu.io.key","-in","ci/sozu.io.csr","-req","-days","365","-out","ci/sozu.io.pem"])
             .with_service_binding("service_receiver", service_receiver)
             # Launch sozu
             .with_exec(["/bin/sozu", "start", "-c", "ci/test.toml"])
@@ -110,11 +115,12 @@ async def test():
         test_run_https_800 = await (
             client.container()
             .from_("fedora:39")
-            .with_exec(["dnf", "install", "-y", "openssl"])
+            .with_exec(["dnf","install","-y","openssl","curl"])
             # Retrieve executable
             .with_file("/bin/bombardier", container_bombardier.file("/app/bombardier"))
             .with_service_binding("sozu.io", service_sozu)
             .with_env_variable("CACHEBUSTER", str(datetime.now()))
+            .with_exec(("curl","-vvv","-k","https://sozu.io:8443/api"))
             .with_exec(["/bin/bombardier", "-c", "800", "-d", "60s", "-k", "https://sozu.io:8443/api"])
             .stdout()
         )
