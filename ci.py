38fafcf62be7b2c567260399ee14b2d7e0821fe9
GuillaumeASSIER <guillaume.assier@clever-cloud.com>
2023-11-23 17:51:01 +0100

    WIP Async😬

--- ci.py
+++ ci.py
@@ -1,14 +1,10 @@
 import sys
+import time
 from datetime import datetime
+from anyio import create_task_group
 
 import anyio
 import dagger
-
-SOZU_CONFIG = """
-
-
-"""
-
 
 async def test():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
@@ -37,7 +33,7 @@ async def test():
             client.container()
             .from_("fedora:39")
             .with_exec(["dnf", "install","-y","rust","cargo"])
-            .with_directory("/app", client.git("https://github.com/Keksoj/lagging_server").branch("main").tree())
+            .with_directory("/app", client.git("https://github.com/Sykursen/lagging_server").branch("main").tree())
             # Cache
             .with_mounted_cache("~/.cargo/registry", client.cache_volume("container_receiver_registry"))
             .with_mounted_cache("~/.cargo/git", client.cache_volume("container_receiver_git"))
@@ -71,64 +67,49 @@ async def test():
             tg.start_soon(container_executor, container_receiver)
 
         ######################
-        # Services definitions
+        # Run
         ######################
+
+        async def command_executor(command):
+            await command
 
         service_receiver = (
             client.container()
             .from_("fedora:39")
-            # Retrieve executable
-            .with_file(
-                "/bin/lagging_server",
-                container_receiver.file(
-                    "/app/target/release/lagging_server"
-                ),
-            )
-            .with_exec(["/bin/lagging_server", "--port", "4444"])
-            .with_exposed_port(4444)
-            .as_service()
-        )
-
-        service_sozu = (
-            client.container()
-            .from_("fedora:39")
-            .with_exec(["dnf", "install", "-y", "openssl"])
-            # Retrieve executable
-            .with_file("/bin/sozu", container_sozu.file("/app/target/release/sozu"))
+            .with_exec(["dnf","install","-y","openssl","tmux"])
             # Mount CI
             .with_directory("/app", await client.host().directory(".", include=["ci/test.toml"]))
             .with_workdir("/app")
             .with_exec(["openssl", "req", "-newkey","rsa:2048","-nodes","-keyout","ci/sozu.io.key","-out","ci/sozu.io.csr","-subj","/CN=sozu.io"])
             .with_exec(["openssl", "x509", "-signkey","ci/sozu.io.key","-in","ci/sozu.io.csr","-req","-days","365","-out","ci/sozu.io.pem"])
-            .with_service_binding("service_receiver", service_receiver)
-            # Launch sozu
-            .with_exec(["/bin/sozu", "start", "-c", "ci/test.toml"])
-            .with_exposed_port(8080)
-            .with_exposed_port(8443)
-            .as_service()
-        )
-
-        ##################
-        # Define tests
-        ##################
-
-        test_run_https_800 = await (
-            client.container()
-            .from_("fedora:39")
-            .with_exec(["dnf","install","-y","openssl","curl"])
-            # Retrieve executable
+            # Retrieve executables
+            .with_file("/bin/lagging_server", container_receiver.file("/app/target/release/lagging_server"))
+            .with_file("/bin/sozu", container_sozu.file("/app/target/release/sozu"))
             .with_file("/bin/bombardier", container_bombardier.file("/app/bombardier"))
-            .with_service_binding("sozu.io", service_sozu)
-            .with_env_variable("CACHEBUSTER", str(datetime.now()))
-            .with_exec(("curl","-vvv","-k","https://sozu.io:8443/api"))
-            .with_exec(["/bin/bombardier", "-c", "800", "-d", "60s", "-k", "https://sozu.io:8443/api"])
-            .stdout()
+            # .as_service()
+            # .start()
         )
-        
-    ##################
-    # Run tests
-    ##################
 
-    print(test_run_https_800)
+        async def lagging_server():
+            return service_receiver.with_exec(["/bin/lagging_server","--port","4444"])
+        
+        async def sozu():
+            return service_receiver.with_exec(["/bin/sozu","start","-c","ci/test.toml"])
+
+        async def bombardier():
+            time.sleep(3)
+            # Bypass cache
+            return (
+                service_receiver.with_env_variable("CACHEBUSTER", str(datetime.now()))
+                                .with_exec(("curl","-vvv","-k","https://sozu.io:8443/api"))
+                                .with_exec(["/bin/bombardier","-c","800","-d","60s","-k","https://sozu.io:8443/api"])
+            )
+
+        async with create_task_group() as tg:
+            tg.start_soon(command_executor, lagging_server())
+            tg.start_soon(command_executor, sozu())
+            tg.start_soon(command_executor, bombardier())
+        
+        #service_receiver.stop()
 
 anyio.run(test)
